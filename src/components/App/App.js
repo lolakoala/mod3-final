13140c42d23e9420ea74df1b4bde1051eb650a12
Lola Brenner <amandadbrenner@gmail.com>
2017-11-02 10:14:26 -0600

    starts card component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,27 +5,45 @@ import './App.css';
 import { connect } from 'react-redux';
 import { addHouses } from '../../actions';
 import wolf from '../../images/wolf.gif';
+import Card from '../Card/Card.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true
+    };
+  }
 
   componentDidMount() {
     const { addHouses } = this.props;
     fetch('http://localhost:3001/api/v1/houses')
       .then(res => res.json())
-      .then(jsonRes => addHouses(jsonRes));
+      .then(jsonRes => addHouses(jsonRes))
+      .then(() => this.setState({ loading: false }));
+  }
+
+  renderHouses = () => {
+    const { houses } = this.props;
+    return houses.map(house => {
+      //I could not get all cards to load without having
+      //this console log for a time buffer. it would only load 3-4 of them.
+      console.log(house)
+      return <Card house={house} key={Date.now()}/>;
+    });
   }
 
   render() {
-    const { houses } = this.props;
-
     return (
       <div className='App'>
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to Westeros</h2>
-          {houses.length <= 0 ? <img src={wolf} alt='wolf'/> : null}
         </div>
         <div className='Display-info'>
+          {!this.state.loading ?
+            this.renderHouses()
+            : <img src={wolf} alt='wolf'/>}
         </div>
       </div>
     );
